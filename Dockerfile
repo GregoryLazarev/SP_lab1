4a4e6ea4c8ee74bd79a319d990fde131e202327b
Gregory <39768466+GregoryLazarev@users.noreply.github.com>
2020-03-02 23:15:33 +0700

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM centos
+COPY lab1.sh /home/lab1.sh
+COPY test.txt /home/test.txt
---
+++ lab1.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+echo -e "\nПрограмма проверки изменений"
+echo "Данная программа позволяет проверить, изменялось ли содержимое файла после указанной даты"
+echo "Разработчик: Лазарев Григорий"
+trig=1
+
+while ( [ "$trig" -ne 0 ] )
+do
+	echo -e "\nВы находитесь в дирректории $(pwd) \nВведите имя файла"
+	read fname
+	f=0
+	while [ $f -eq 0 ]
+	do
+		if [ ! -e $fname ]
+		then
+			echo "Неверно введено имя афйла"
+			echo -e "Хотите продолжить? [Y/n]"
+			t=0
+			while [ $t -eq 0 ]
+			do
+				read des			
+				case "$des" in
+					y|Y) t=1
+						 echo "Введите имя файла"
+						 read fname
+						 ;;
+					n|N) t=2
+						 exec 2>>errfile
+		 				 echo "Неверно введено имя афйла" >&2
+						 exit 120
+						 ;;
+					*) echo -e "Хотите продолжить? [Y/n]"
+						 ;;
+				esac
+			done			
+		else
+			f=1
+		fi
+	done
+
+	echo "Введите дату в формате гггг-мм-дд"
+	read u_date
+	fdate=$(date -r $fname +%s)
+	udate=$(date -d $u_date +%s)
+
+	if [ $fdate -gt $udate ]
+	then
+		echo "Содержимое файла изменялось после указанной даты"
+	else
+		echo "Содержимое файла не изменялось после указанной даты"
+	fi
+	echo -e "\n1	продолжить\n0	выход"
+	
+	t=0
+	while [ $t -eq 0 ]
+	do
+		read des			
+		case "$des" in
+			0) 	t=1
+				trig=0
+				;;
+			1)	t=2
+				;;
+			*) 	echo -e "\n1	продолжить\n0	выход"
+				;;
+		esac
+	done
+	
+done
